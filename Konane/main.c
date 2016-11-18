c70dc268f6f7ed9242d1295944a8be82eb46c19b
Melissa van Staalduinen <vanstaalduinenm@Melissas-MacBook-Air.local>
2016-11-17 22:37:45 -0700

    board initialization.

--- Konane/main.c
+++ Konane/main.c
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   main.c
  * Author: Majo
  *
@@ -15,10 +15,32 @@
 #include <stdlib.h>
 
 /*
- * 
+ *
  */
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
+	// "board.txt", "B"
+	// first arg is name of .txt file
+	// second arg is B or W
+	// PLAYER = c
+
+	char board[8][8];
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			// don't check if null character because board should be right
+			board[i][j] = fgetc(fp);
+		}
+		// get rid of new character line.
+	}
 
     return (EXIT_SUCCESS);
 }
 
+// open file and read board
+
+// store board -- 2D array
+
+//print out move
+//		form X#-X#
+
+// accept move input
