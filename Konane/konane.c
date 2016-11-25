5457142fe687ecc14c6201e60760bb58dea6a64f
Melissa van Staalduinen <vanstaalduinenm@Melissas-MacBook-Air.local>
2016-11-24 21:32:34 -0700

    small fix, still segfaults in gen_move

--- Konane/konane.c
+++ Konane/konane.c
@@ -170,9 +170,7 @@ void update_board(struct state *change)
 	prev = change->board[i][j];
 	change->board[i][j] = 'O';
 
-	if (move[2] != '-') {
-		break;
-	} else {
+	if (move[2] == '-') {
 		// location of square to move to
 		int p = translate_letter(move[3]); // letter is column
 		int q = 8 - (strtol(&move[4], &new, 10)); // number is row.
@@ -226,29 +224,29 @@ char * get_space(int row, int col)
 {
 	char *move;
 
+	move = malloc(sizeof(char)*8);
+
 	move[0] = translate_number(col);
-	if (move[0] == 'Z') {
-		fprintf(stderr, "ERROR: couldn't match number index to letter");
-	}
+	//if (move[0] == 'Z') {
+	//	fprintf(stderr, "ERROR: couldn't match number index to letter");
+
 	int i = 8 - row;
 	move[1] = (char)(i + '0');
-
 	return move;
 }
 	/* get the move that can be performed given the current board and empty space
 	 * return as string LetterNumber-LetterNumber
 	  * which is the move
 	 */
-char *gen_move(struct state *original, char *open int row, int col, char *direction)
+char *gen_move(struct state *original, char *open, int row, int col, char *direction)
 {
 	// check which color is above/below/right/left of it
 	char *move;
 	int indx;
-
-	move = malloc(sizeof(char)*8);
+	move = malloc(sizeof(char)*18);
 
 	if (strcmp(direction, "up") == 0) {
-		if (original->board[row-1][col] == player) {
+		if ((original->board[row-1][col] == 'O') || (original->board[row-1][col] == player)) {
 			/* if the piece beside the open spot is the player
 			 * then the player cannot move into the open spot
 			*/
@@ -256,38 +254,43 @@ char *gen_move(struct state *original, char *open int row, int col, char *direct
 		} else {
 			indx = row - 2;
 			move = get_space(indx, col);
-			move[2] = "-";
+				fprintf(stdout, "- 1 %s  - \n", move);
+			move[2] = '-';
 			strcat(move, open);
 		}
 	} else if (strcmp(direction, "down") == 0) {
-		if (original->board[row+1][col] == player) {
+		if ((original->board[row+1][col] == 'O') || (original->board[row+1][col] == player)) {
 			return move = "invalid";
 		} else {
 			indx = row + 2;
 			move = get_space(indx, col);
-			move[2] = "-";
+					fprintf(stdout, "-2  %s  - \n", move);
+			move[2] = '-';
 			strcat(move, open);
 		}
 	} else if (strcmp(direction, "left") == 0) {
-		if (original->board[row][col-1] == player) {
+		if ((original->board[row][col-1] == 'O') || (original->board[row][col-1] == player)) {
 			return move = "invalid";
 		} else {
 			indx = col - 2;
-			move = get_space(indx, col);
-			move[2] = "-";
+			move = get_space(row, indx);
+					fprintf(stdout, "- 3 %s  - \n", move);
+			move[2] = '-';
 			strcat(move, open);
 		}
 	} else if (strcmp(direction, "right") == 0) {
-		if (original->board[row][col+1] == player) {
+		if ((original->board[row][col+1] == 'O') || (original->board[row][col+1] == player)) {
 			return move = "invalid";
 		} else {
 			indx = col + 2;
-			move = get_space(indx, col);
-			move[2] = "-";
+			move = get_space(row, indx);
+					fprintf(stdout, "- 4  %s  - \n", move);
+			move[2] = '-';
 			strcat(move, open);
 		}
-
 	}
+	fprintf(stdout, "!!!  %s  !!!! \n", move);
+	return move;
 }
 
 /**
@@ -342,7 +345,7 @@ struct state **get_moves (struct state *current, int *count)
 		states[1] = state2;
 	}
 
-	char direction[4] = {"up", "down", "left", "right"};
+	const char *direction[] = {"up", "down", "left", "right"};
 
 
 	if (empty > 1) {
@@ -353,6 +356,7 @@ struct state **get_moves (struct state *current, int *count)
 			for (int j = 0; j < BOARD_SIZE; j++) {
 				if (current->board[i][j] == 'O') {
 					char *open = get_space(i, j);
+					fprintf (stdout, "%s ---- \n", open);
 					for (int k = 0; k < 4; k++) {
 						char *tomove = gen_move(current, open, i, j, direction[k]);
 						if (strcmp(tomove, "invalid") != 0) {
